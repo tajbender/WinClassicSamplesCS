c4454f19b5883e6d1ad7f1650ee259c339eb8728
dahall <dahall.dev@gmail.com>
2025-06-24 17:22:25 -0600

    Updated SimpleDiction sample

--- Win7Samples/winui/speech/SimpleDictation/SimpleDictation.cs
+++ Win7Samples/winui/speech/SimpleDictation/SimpleDictation.cs
@@ -1,13 +1,13 @@
 ﻿using Vanara;
-using Vanara.Extensions;
 using Vanara.InteropServices;
 using Vanara.PInvoke;
-using static Vanara.PInvoke.Kernel32;
 using static Vanara.PInvoke.User32;
 using static Vanara.PInvoke.SpeechApi;
 
-namespace SimpleDictation;
-internal class CSimpleDict
+// Main entry point for the application
+new CSimpleDict().Show();
+
+internal class CSimpleDict : ModalDialog
 {
 	// resource.h
 	const int IDD_SIMPLEDICTDLG = 100;
@@ -21,57 +21,41 @@ internal class CSimpleDict
 	bool m_bGotReco;
 	ISpRecoContext? m_cpRecoCtxt;
 	ISpRecoGrammar? m_cpDictationGrammar;
-	SafeHINSTANCE g_hInst;
-	private HWND m_hDlg;
 
-	internal static void Main() => new CSimpleDict().Run();
-
-	public CSimpleDict() => Win32Error.ThrowLastErrorIfInvalid(g_hInst = LoadLibraryEx("SimpleDictationRes.dll", LoadLibraryExFlags.LOAD_LIBRARY_AS_DATAFILE));
-
-	public void Run() => DialogBox(g_hInst, IDD_SIMPLEDICTDLG, HWND.NULL, SimpleDictDlgProc);
+	public CSimpleDict() : base("SimpleDictationRes.dll", IDD_SIMPLEDICTDLG) { }
 
 	/*********************************************************************************************
-	* CSimpleDict::SimpleDictDlgProc()
+	* CSimpleDict::InternalDialogProc()
 	*       Main message handler for SimpleDictation dialog box.
 	*       At initialization time, LPARAM is the CSimpleDict object, to which the
 	*       window long is set.
 	**********************************************************************************************/
-	private nint SimpleDictDlgProc(HWND hDlg, uint message, nint wParam, nint lParam)
+	protected override nint DialogProc(HWND hDlg, uint message, nint wParam, nint lParam)
 	{
-		switch ((WindowMessage)message)
+		if ((WindowMessage)message == WM_RECOEVENT)
 		{
-			case WindowMessage.WM_INITDIALOG:
-
-				if (!InitDialog(hDlg))
-					DestroyWindow(hDlg);
-				break;
-
-			case WM_RECOEVENT:
-				// All recognition events send this message, because that is how we
-				// specified we should be notified
-				RecoEvent();
-				return 1;
-
-			case WindowMessage.WM_COMMAND:
-				if (Macros.LOWORD(wParam) == IDC_BUTTON_EXIT)
-					EndDialog(hDlg, 1);
-				break;
-
-			case WindowMessage.WM_CLOSE:
-				EndDialog(hDlg, 1);
-				break;
-
-			case WindowMessage.WM_DESTROY:
-				// Release the recognition context and the dictation grammar
-				m_cpRecoCtxt = null;
-				m_cpDictationGrammar = null;
-				break;
-
-			default:
-				return 0;
+			// All recognition events send this message, because that is how we
+			// specified we should be notified
+			RecoEvent();
+			return 1;
 		}
 
-		return 1;
+		return 0;
+	}
+
+	protected override nint OnClosing() => 1;
+
+	protected override void OnCommand(int id, HWND hControl, uint code)
+	{
+		if (id == IDC_BUTTON_EXIT)
+			Close();
+	}
+
+	protected override void OnDestroying()
+	{
+		// Release the recognition context and the dictation grammar
+		m_cpRecoCtxt = null;
+		m_cpDictationGrammar = null;
 	}
 
 	/*********************************************************************************************
@@ -79,10 +63,8 @@ internal class CSimpleDict
 	* Creates the recognition context and activates the grammar. 
 	* Returns true iff successful.
 	**********************************************************************************************/
-	bool InitDialog(HWND hDlg)
+	protected override bool OnInitDialog()
 	{
-		m_hDlg = hDlg;
-
 		try
 		{
 			ISpRecognizer cpRecoEngine = (ISpRecognizer)new SpInprocRecognizer();
@@ -90,7 +72,7 @@ internal class CSimpleDict
 			m_cpRecoCtxt = cpRecoEngine.CreateRecoContext();
 
 			// Set recognition notification for dictation
-			m_cpRecoCtxt.SetNotifyWindowMessage(hDlg, (uint)WM_RECOEVENT, 0, 0);
+			m_cpRecoCtxt.SetNotifyWindowMessage(Handle, (uint)WM_RECOEVENT, 0, 0);
 
 			// This specifies which of the recognition events are going to trigger notifications.
 			// Here, all we are interested in is the beginning and ends of sounds, as well as
@@ -146,7 +128,7 @@ internal class CSimpleDict
 							// but the engine has not succeeded in recognizing anything
 							SafeLPTSTR szNoise = new("<noise>");
 
-							SendDlgItemMessage(m_hDlg, IDC_EDIT_DICT, (uint)EditMessage.EM_REPLACESEL, (BOOL)true, (IntPtr)szNoise);
+							SendDlgItemMessage(Handle, IDC_EDIT_DICT, (uint)EditMessage.EM_REPLACESEL, (BOOL)true, (IntPtr)szNoise);
 						}
 						m_bGotReco = false;
 					}
@@ -165,7 +147,7 @@ internal class CSimpleDict
 						// Concatenate a space onto the end of the recognized word
 						dstrText += " ";
 
-						SendDlgItemMessage(m_hDlg, IDC_EDIT_DICT, (uint)EditMessage.EM_REPLACESEL, (BOOL)true, new SafeLPTSTR(dstrText));
+						SendDlgItemMessage(Handle, IDC_EDIT_DICT, (uint)EditMessage.EM_REPLACESEL, (BOOL)true, new SafeLPTSTR(dstrText));
 					}
 					break;
 
