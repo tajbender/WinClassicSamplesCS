97aa6454b3bacaaaa2e7f42dc38e342b3394cf06
Thorsten Jung <tajbender@users.noreply.github.com>
2026-03-27 09:59:21 +0100

    Add package `CommunityToolkit.Mvvm` Add `IShellItemModel`, `IShellFolderModel`, `ShellItemModel`

--- WinClassicSamplesBrowser/Models/ShellItemModel.cs
+++ WinClassicSamplesBrowser/Models/ShellItemModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,54 @@ using System.Threading.Tasks;
 
 namespace WinClassicSamplesBrowser.Models;
 
-internal class ShellItemModel
+public interface IShellItemModel
 {
+    string Name { get; }
+    string? DisplayName { get; }
+    string? ParsingName { get; }
+    string? TypeName { get; }
+    bool IsFolder { get; }
+    Task<IconSource?> GetIconAsync();
+}
+
+public interface IShellFolderModel : IShellItemModel
+{
+    Task<IReadOnlyList<IShellItemModel>> GetChildrenAsync();
+}
+
+public class ShellItemModel : ObservableObject, IShellItemModel
+{
+    public string Name { get; }
+    public string? DisplayName { get; }
+    public string? ParsingName { get; }
+    public string? TypeName { get; }
+    public bool IsFolder { get; }
+
+    private readonly Func<Task<IconSource?>> _iconLoader;
+    private IconSource? _icon;
+
+    public ShellItemModel(
+        string name,
+        string? displayName,
+        string? parsingName,
+        string? typeName,
+        bool isFolder,
+        Func<Task<IconSource?>> iconLoader)
+    {
+        Name = name;
+        DisplayName = displayName;
+        ParsingName = parsingName;
+        TypeName = typeName;
+        IsFolder = isFolder;
+        _iconLoader = iconLoader;
+    }
+
+    public async Task<IconSource?> GetIconAsync()
+    {
+        if (_icon != null)
+            return _icon;
+
+        _icon = await _iconLoader();
+        return _icon;
+    }
 }
