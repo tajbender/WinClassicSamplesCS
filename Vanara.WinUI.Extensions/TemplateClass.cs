4950c0d112c7fc3ccdae873eb5bc82c2aabb76a4
Thorsten Jung <tajbender@users.noreply.github.com>
2026-04-03 21:10:43 +0200

    Add project `Vanara.WinUI.Extensions` to Solution, including `TemplateClass`

---
+++ Vanara.WinUI.Extensions/TemplateClass.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanara.WinUI.Extensions;
+
+public partial class TemplateClass
+{
+}
