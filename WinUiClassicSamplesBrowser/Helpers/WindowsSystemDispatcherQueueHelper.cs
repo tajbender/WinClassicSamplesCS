a7ee956a05277f4d6d259aa9fa983b66b77c1f75
Thorsten Jung <tajbender@users.noreply.github.com>
2026-04-11 22:37:56 +0200

    Add `AboutPage` Add `HomePage` Add `MainPage` Add `SamplesPage` Add `SettingsPage`

---
+++ WinUiClassicSamplesBrowser/Helpers/WindowsSystemDispatcherQueueHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Dispatching;
+
+namespace ClassicSamplesBrowser.Helpers;
+
+public class WindowsSystemDispatcherQueueHelper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct DispatcherQueueOptions
+    {
+        public int dwSize;
+        public int threadType;
+        public int apartmentType;
+    }
+
+    [DllImport("CoreMessaging.dll")]
+    private static extern int CreateDispatcherQueueController(
+        DispatcherQueueOptions options,
+        out IntPtr dispatcherQueueController);
+
+    private IntPtr _dispatcherQueueController = IntPtr.Zero;
+
+    public void EnsureWindowsSystemDispatcherQueueController()
+    {
+        if (DispatcherQueue.GetForCurrentThread() != null)
+        {
+            // Already initialized
+            return;
+        }
+
+        if (_dispatcherQueueController == IntPtr.Zero)
+        {
+            DispatcherQueueOptions options = new DispatcherQueueOptions
+            {
+                dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions)),
+                threadType = 2,      // DQTYPE_THREAD_CURRENT
+                apartmentType = 2    // DQTAT_COM_STA
+            };
+
+            CreateDispatcherQueueController(options, out _dispatcherQueueController);
+        }
+    }
+}
--- WinUiClassicSamplesBrowser/MainWindow.xaml.cs
+++ WinUiClassicSamplesBrowser/MainWindow.xaml.cs
@@ -1,11 +1,56 @@
+using ClassicSamplesBrowser.Helpers;
+using ClassicSamplesBrowser.Views;
+using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WinRT;
 
 namespace ClassicSamplesBrowser;
 
 public sealed partial class MainWindow : Window
 {
+    private WindowsSystemDispatcherQueueHelper _wsdqHelper;
+    private MicaController _micaController;
+    private SystemBackdropConfiguration _backdropConfig;
+
     public MainWindow()
     {
-        this.InitializeComponent();
+        InitializeComponent();
+        TrySetMicaBackdrop();
+
+        RootFrame.Navigate(typeof(MainPage));
+    }
+
+    private void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        if (_backdropConfig != null)
+        {
+            _backdropConfig.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+    }
+
+    private bool TrySetMicaBackdrop()
+    {
+        if (!MicaController.IsSupported())
+            return false;
+
+        _wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+        _wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+        _backdropConfig = new SystemBackdropConfiguration
+        {
+            IsInputActive = true,
+            Theme = SystemBackdropTheme.Default
+        };
+
+        _micaController = new MicaController
+        {
+            Kind = MicaKind.BaseAlt
+        };
+
+        _micaController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+        _micaController.SetSystemBackdropConfiguration(_backdropConfig);
+
+        return true;
     }
 }
