2cf269ef124e1a475e0592c793fe3e77d8019dc6
Thorsten Jung <tajbender@users.noreply.github.com>
2026-04-13 19:59:23 +0200

    Add `ApiExplorerPage` and `StartPage`

--- WinUiClassicSamplesBrowser/Views/ApiExplorerPage.xaml.cs
+++ WinUiClassicSamplesBrowser/Views/ApiExplorerPage.xaml.cs
@@ -8,9 +8,9 @@ namespace ClassicSamplesBrowser.Views;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class HomePage : Page
+public sealed partial class ApiExplorerPage : Page
 {
-    public HomePage()
+    public ApiExplorerPage()
     {
         InitializeComponent();
     }
