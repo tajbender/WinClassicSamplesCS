bffe61e420c38cf632b313ba9b89cd24433e5b91
Thorsten Jung <tajbender@users.noreply.github.com>
2026-04-13 15:15:49 +0200

    Remove `NavigationBar`

--- WinUiClassicSamplesBrowser/Views/MainPage.xaml.cs
+++ WinUiClassicSamplesBrowser/Views/MainPage.xaml.cs
@@ -8,32 +8,33 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         InitializeComponent();
-        ContentFrame.Navigate(typeof(HomePage));
+
+        //ContentFrame.Navigate(typeof(HomePage));
     }
 
-    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-    {
-        if (args.SelectedItem is NavigationViewItem item)
-        {
-            var tag = item.Tag?.ToString();
-
-            switch (tag)
-            {
-                case "HomePage":
-                    ContentFrame.Navigate(typeof(HomePage));
-                    HeaderTitle.Text = "Home";
-                    break;
-
-                case "SamplesPage":
-                    ContentFrame.Navigate(typeof(SamplesPage));
-                    HeaderTitle.Text = "Samples";
-                    break;
-
-                case "AboutPage":
-                    ContentFrame.Navigate(typeof(AboutPage));
-                    HeaderTitle.Text = "About";
-                    break;
-            }
-        }
-    }
+//    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+//    {
+//        if (args.SelectedItem is NavigationViewItem item)
+//        {
+//            var tag = item.Tag?.ToString();
+//
+//            switch (tag)
+//            {
+//                case "HomePage":
+//                    ContentFrame.Navigate(typeof(HomePage));
+//                    //HeaderTitle.Text = "Home";
+//                    break;
+//
+//                case "SamplesPage":
+//                    ContentFrame.Navigate(typeof(SamplesPage));
+//                    //HeaderTitle.Text = "Samples";
+//                    break;
+//
+//                case "AboutPage":
+//                    ContentFrame.Navigate(typeof(AboutPage));
+//                    //HeaderTitle.Text = "About";
+//                    break;
+//            }
+//        }
+//    }
 }
